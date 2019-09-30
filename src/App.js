26609f8efbb90f4dab1ebb50e734548f4d6d095d
ProUnebit <ProUnebit@yandex.ru>
2019-09-30 20:46:00 +0300

    update menu-items, added routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom'
+
 import "./App.css";
 
 import HomePage from './pages/Homepage/Homepage.component';
 
+function HatsPage() {
+    return (
+        <div>
+            <h1>HATS PAGE</h1>
+        </div>
+    )
+}
+
 function App() {
     return (
         <div className="App">
-            <HomePage />
+            <Switch>
+                <Route exact path='/' component={HomePage} />
+                <Route path='/hats' component={HatsPage} />
+            </Switch>
         </div>
     );
 }
